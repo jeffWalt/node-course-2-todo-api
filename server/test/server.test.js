700d747f49c1361193726ea7c10d8f63a3c70af4
Your Name <you@example.com>
2018-02-05 04:00:31 -0500

    Added tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,12 +4,20 @@ const request = require("supertest");
 var {app} = require("./../server");
 var {Todo} = require("./../models/todo");
 
+const todos = [
+  {text: "First test todo"},
+  {text: "Second test todo"}
+];
 beforeEach((done) => { // Runs everytime there is a test case called
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos); // This returns a call back so we can chain promises
+  }).then(() => {
+    done();
+  })
 });
 
 describe("POST /todos", () =>{
-  it("should create a new todo", (done) => {
+  it("Should create a new todo", (done) => {
     var text = "Test todo text";
     request(app)
       .post("/todos")
@@ -21,7 +29,7 @@ describe("POST /todos", () =>{
       .end((error, res) => { // See if request was errored, if not then check if the database has our document that we inputted
         if (error) return done(error) // Will print the error
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -39,9 +47,21 @@ describe("POST /todos", () =>{
         if (error) return done(error)
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(2)
           done()
         }).catch((error) => done(error));
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("Should get all todos", (done) => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+    .end(done);
   });
 });
