92d48fd2e8350a076e7cb641e15a456d6e71b158
Your Name <you@example.com>
2018-02-05 05:12:27 -0500

    Added /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require('body-parser');
+const {ObjectID} = require("mongodb");
 
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
@@ -20,13 +21,28 @@ app.post("/todos", (req, res) => {
   })
 });
 
-
 app.get("/todos", (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (error) => {
     res.status(400).send(error);
   });
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo){
+      return res.status(404).send();
+    }
+    return res.send({todo});
+  }, (e) => {
+    return res.status(400).send(e);
+  }).catch((e) => console.log(e))
 });
 
 app.listen(3000, () => {
