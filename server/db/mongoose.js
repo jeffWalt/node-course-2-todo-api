76afbda08a046d1340697cdf1a6657fbb66fce95
Your Name <you@example.com>
2018-02-07 05:45:39 -0500

    Added post users route and token method?

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise // Sets it so we can use promises for mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
+require("./config/config.js");
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const {ObjectID} = require("mongodb");
 const _ = require("lodash");
 
@@ -7,7 +8,7 @@ var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 var app = express();
 app.use(bodyParser.json()); //Middleware
@@ -80,13 +81,25 @@ app.patch("/todos/:id", (req, res) => {
     body.completed = false;
     body.completedAt = null;
   }
-  
+
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo){
       return res.status(404).send()
     }
     res.send({todo});
   }).catch((e) => res.status(404).send());
+});
+
+app.post("/users", (req, res) => {
+  const body = _.pick(req.body, ["email", "password"])
+  const user = new User(body)
+
+  user.save().then(() => {
+    
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header("x-auth", token).send(user)
+  }).catch((e) => res.status(400).send(e));
 });
 
 app.listen(port, () => {
